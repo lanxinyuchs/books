3d8f6f2608a1de938fee0ac42859a6552308e1e8
ezlanxi <xinyu.lan@ericsson.com>
2017-11-15 04:01:59 +0100

    none

--- rct/scripts/tb_source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/home/sirpa/testbins/
-export LD_LIBRARY_PATH=/home/sirpa/testlibs:/home/sirpa/testlibs/ngns:/home/sirpa/testlibs/liblits:/home/sirpa/testlibs/capi
-export LITS_DIR=/home/sirpa/testbins
-export CAPI_LOG_PATH=/var/log
--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Installing git hooks."
-$(dirname $0)/.git_hooks_wrapper
